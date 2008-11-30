2c319557ebdd2df4bf83cf198598bc45f0b3cc6c
kaleissin <kaleissin@72aaf930-848c-11dd-ac5a-f353d88c4ef8>
2008-11-30 15:40:03 +0000

    Better overview in the ist of pms

--- nano/privmsg/admin.py
+++ nano/privmsg/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 
 from nano.privmsg.models import PM
 
-# class PMAdmin(admin.ModelAdmin): 
-#     model = PM
-#     ordering = ('subject', '-sent')
-#     list_display = ('sender', 'recipient', 'subject', 'sent')
-#     list_filter = ('sender', 'recipient',)
-#     search_fields = ('sender', 'recipient', 'subject', 'sent')
-#admin.site.register(PM, PMAdmin)
-admin.site.register(PM)
+class PMAdmin(admin.ModelAdmin): 
+    model = PM
+    ordering = ('subject', '-sent')
+    list_display = ('subject', 'sender', 'recipient', 'sent',
+    'recipient_archived', 'recipient_deleted', 'sender_deleted')
+    list_filter = ('sender', 'recipient',)
+    search_fields = ('sender', 'recipient', 'subject', 'sent')
+admin.site.register(PM, PMAdmin)
 
