68684fb9233bc112a979394ef29477b3c78af101
kaleissin <kaleissin@72aaf930-848c-11dd-ac5a-f353d88c4ef8>
2009-08-21 15:29:54 +0000

    Show pub_date in admin-list

--- nano/blog/admin.py
+++ nano/blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 
 from nano.blog.models import *
 
-admin.site.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    model = Entry
+    list_display = ('headline', 'pub_date')
+
+admin.site.register(Entry, EntryAdmin)
