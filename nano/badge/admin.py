1b342f85123e27ec1c15954a2cfd5421265a0a46
kaleissin <kaleissin@72aaf930-848c-11dd-ac5a-f353d88c4ef8>
2010-01-08 22:17:46 +0000

    Better admin-display

--- nano/badge/admin.py
+++ nano/badge/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from nano.badge.models import Badge
 
 class BadgeAdmin(admin.ModelAdmin):
-    list_display = ('level', 'name')
+    list_display = ('level', 'name', 'description')
     list_filter = ('level',)
     ordering = ('level', 'name',)
 admin.site.register(Badge, BadgeAdmin)
