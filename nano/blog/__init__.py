ee772b0d525650fc48313db6e8610db270625ebb
kaleissin <kaleissin@72aaf930-848c-11dd-ac5a-f353d88c4ef8>
2010-01-08 22:25:50 +0000

    Better duplication-detection in add_entry_to_blog()

--- nano/blog/__init__.py
+++ nano/blog/__init__.py
@@ -1,12 +1,18 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.template.loader import render_to_string
 
 from nano.blog.models import Entry as _Entry
 
-def add_entry_to_blog(object, headline, template, date_field=None):
-    data = {'obj': object}
-    template = render_to_string(template, dictionary=data)
-    pub_date = object.__dict__.get(date_field or 'last_modified', datetime.now())
-    blog_entry = _Entry.objects.create(content=template,headline=headline,pub_date=pub_date)
+_five_minutes = timedelta(seconds=5*60)
 
+def add_entry_to_blog(object, headline, template, date_field='last_modified'):
+    data = {'obj': object}
+    current_time = datetime.now()
+    template = render_to_string(template, dictionary=data)
+    pub_date = object.__dict__.get(date_field, current_time)
+    latest = _Entry.objects.latest()
+    # Prevent duplicates
+    if not (latest.headline == headline and latest.pub_date > current_time - _five_minutes):
+        blog_entry = _Entry.objects.create(content=template,headline=headline,pub_date=pub_date)
+        return blog_entry
