22120a1ad9d624b0c4fda8fed04c48f362fdfcbe
kaleissin <kaleissin@gmail.com>
2013-09-15 18:13:09 +0200

    nano.privmsg free of functional generic views

--- nano/privmsg/views.py
+++ nano/privmsg/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.views.generic.list_detail import object_list
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
