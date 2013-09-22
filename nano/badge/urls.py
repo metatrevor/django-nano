868b9c1d10b594cfceec0409758337870c3b4d6b
kaleissin <kaleissin@gmail.com>
2013-09-22 23:13:41 +0200

    Generic CBVs not quite compatible...

--- nano/badge/urls.py
+++ nano/badge/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('nano.badge.views',
         url(r'^$',                        'list_badges', {}, 'badge-list'),
-        url(r'^(?P<object_id>[0-9]+)/$',  'show_badge', {}, 'badge-detail'),
+        url(r'^(?P<pk>[0-9]+)/$',  'show_badge', {}, 'badge-detail'),
 )
