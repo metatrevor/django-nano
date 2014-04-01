3307ceaf1a2fad0cf1b97ba888847facc18b7d9e
kaleissin <kaleissin@gmail.com>
2014-04-01 12:35:13 +0200

    Bugfix-release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ packages=(
 )
 
 setup(name='nano',
-        version='0.7.2',
+        version='0.7.3',
         packages=find_packages(exclude=exclude),
         include_package_data=True,
         zip_safe=False,
