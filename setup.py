31575bf2debe43d6b1085b1783d5921df570b923
kaleissin <kaleissin@gmail.com>
2014-09-14 22:22:59 +0200

    Upped version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ packages=(
 )
 
 setup(name='nano',
-        version='0.7.3',
+        version='0.7.4',
         packages=find_packages(exclude=exclude),
         include_package_data=True,
         zip_safe=False,
