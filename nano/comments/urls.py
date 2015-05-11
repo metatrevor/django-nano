caf768769bbd99a194047da4e5c46320770e4b51
kaleissin <kaleissin@gmail.com>
2015-05-11 21:49:56 +0200

    Groan

--- nano/comments/urls.py
+++ nano/comments/urls.py
@@ -6,13 +6,11 @@ except ImportError:
     # Django < 1.8
     from django.views.defaults import shortcut
 
-from nano.comments.models import *
-
-comment_dict = {}
+from nano.comments import views
 
 
 urlpatterns = patterns('',
-    url(r'^$',         views.list_comments, comment_dict, name='comments-list-comments'),
+    url(r'^$',         views.list_comments, name='comments-list-comments'),
     url(r'^post$',     views.post_comment, name='comments-post-comment'),
 )
 
